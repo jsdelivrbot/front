deb039067117650e7246deb472968b1376043b5a
jherson <jherson.o.s@hotmail.com>
2016-06-23 12:02:47 -0500

    fix

--- chat/js/chat.js
+++ chat/js/chat.js
@@ -5,8 +5,8 @@ setTimeout(function(){
      },2000)
 },1000)
 function addclass(){
-	$('#diloo-wjs').removeClass('chat-large');
-	$('#diloo-wjs').addClass('chat-larges');
+	$('#d').removeClass('chat-large');
+	$('#d').addClass('chat-larges');
 	//$('#d').removeClass('off');
 	//$('#d').removeClass('animated zoomOut');
 	//$('#d').addClass('animated zoomIn');
@@ -15,8 +15,8 @@ function addclass(){
 	$('.start-chat').removeClass('off');
 	$('.btn-msj').trigger('mouseout');
 }function rmclass(){
-	$('#diloo-wjs').removeClass('chat-larges');
-	$('#diloo-wjs').addClass('chat-large');
+	$('#d').removeClass('chat-larges');
+	$('#d').addClass('chat-large');
 	//$('#d').removeClass('animated zoomIn');
 	//$('#d').addClass('animated zoomOut');
 	$('#chat').show();
@@ -36,24 +36,22 @@ console.log(t);
 }
 
  $(function() {
-  
     // Setup form validation on the #register-form element
     $("#fc").validate({
-    
         // Specify the validation rules
         rules: {
             user: "required",
             last_name:"required"
-        },
-        
+        },      
         // Specify the validation error messages
         messages: {
             user: "Ingrese 1 digito mínimo",
             last_name: "Inrese un correo válido",            
         },
-        
         submitHandler: function(form) {
-            form.submit();
+           console.log("Usuario login");
+           start();
+           //actions.login({name:'miguel',email:'maliaga.pantoja@gmail.com'});
         }
     });
 });
